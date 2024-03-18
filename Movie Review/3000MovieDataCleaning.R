c6051be0001c649cc0d1fcdbea5a50ec0ce0da56
HanzDLC <hanzyeahyuh12@gmail.com>
2024-03-19 00:22:40 +0800

    data cleaning update

--- Movie Review/3000MovieDataCleaning.R
+++ Movie Review/3000MovieDataCleaning.R
@@ -1,18 +1,46 @@
 library(readr)
 library(dplyr)
+library(lubridate)
+library(stringr)
 
-
+#import the csv file into an objcet
 Movies3000DF <- read.csv('MovieReviews.csv')
 
-Movie_X <- Movies3000DF %>%
-  mutate(Date = dmy(Date)) %>%
-  mutate(Date = format(Date, "%d/%m/%Y")
- 
-typeof(Movie_X$Date)      
+#change the format of the date column to date
+Movies3000DF$Date <- dmy(Movies3000DF$Date)
+
+#checking for the data type of date
+class(Movies3000DF$Date)
+
+#converting the star rating from character to 
+as.character(Movies3000DF)
+Movies3000DF$Stars <- gsub("/10", "",Movies3000DF$Stars)
+View(Movies3000DF)
+
+#remove the white spaces and \n in the stars column 
+Remove <- grepl("\\s+|\n", Movies3000DF$Stars)
+Movies3000DF$Stars <- gsub("\\s+|\n", "", Movies3000DF$Stars)
+Movies3000DF$Title <- gsub("\\s+|\n", "", Movies3000DF$Title)
+
+#convert the stars column into a numeric data type
+Movies3000DF$Stars <- as.numeric(Movies3000DF$Stars)
 
 
-Movies_X %>% 
- removeSpace <- grepl("^\n",stars)%>%
- remove10 <-  grepl("^/10",stars)
+View(Movies3000DF)
 
+#lowercase the columns 
+Movies3000DF$Movie_Title <- tolower(Movies3000DF$Movie_Title)
+Movies3000DF$Title <- tolower(Movies3000DF$Title) 
+Movies3000DF$User <- tolower(Movies3000DF$User)
+Movies3000DF$Content <- tolower(Movies3000DF$Content)
 
+#remove emoticons using stringr package
+Movies3000DF$Content <-  str_remove_all(Movies3000DF$Content, "[\\U0001F600-\\U0001F64F]")
+Movies3000DF$Title <- str_remove_all(Movies3000DF$Title, "[\\U0001F600-\\U0001F64F]")
+Movies3000DF$User <- str_remove_all(Movies3000DF$User, "[\\U0001F600-\\U0001F64F]")
+
+View(Movies3000DF)
+
+write.csv(Movies3000DF,file ="Movie3000_DataCleaning.csv")
+
+print(Movies3000DF$Content)
